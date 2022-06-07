b1baee93f94e02f1afd4afac7c45ceefef61b28c
The Kicker <cav.lemasters@geneva.edu>
2022-06-07 10:39:12 -0400

    Fixed GA :bug:

--- assets/js/GA-OPTOUT.js
+++ assets/js/GA-OPTOUT.js
@@ -6,6 +6,7 @@
 
 // Set to the same value as the web property used on the site
 var gaProperty = 'G-6BCK7ND55P';
+var domainValue = "lunco.space"
 
 // Disable tracking if the opt-out cookie exists.
 var disableStr = 'ga-disable-' + gaProperty;
@@ -16,8 +17,8 @@ if (document.cookie.indexOf(disableStr + '=true') > -1) {
 // Opt-out function
 function gaOptout() {
     document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-    document.cookie = '_ga=; path=/; domain='+ domainValue + '; expires=' + new Date(0).toUTCString();
-    document.cookie = '_gid=; path=/; domain='+ domainValue + '; expires=' + new Date(0).toUTCString();
+    document.cookie = '_ga=; path=/; domain=lunco.space; expires=' + new Date(0).toUTCString();
+    document.cookie = '_gid=; path=/; domain=lunco.space; expires=' + new Date(0).toUTCString();
     window[disableStr] = true;
     console.log("Google Analytics tracking has been disabled.")
     window.alert("You have opted out of Google Analytics.")
